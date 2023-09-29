20a68ae8baa4fbf2f00477403bc168ab7ddef953
seulgi <guseul9405@gmail.com>
2023-09-29 22:33:53 +0900

    Feat: 바빌론 기초 세팅 및 예제 코드

---
+++ first-babylonjs/src/examples/MostFirst.jsx
@@ -0,0 +1,34 @@
+import { FreeCamera, Vector3, HemisphericLight, MeshBuilder } from "@babylonjs/core";
+import Secene from "./Secene";
+import SceneComponent from "babylonjs-hook";
+
+let box;
+
+const onSceneReady = (scene) => {
+  const camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+  camera.setTarget(Vector3.Zero());
+
+  const canvas = scene.getEngine().getRenderingCanvas();
+  camera.attachControl(canvas, true);
+
+  const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+  light.intensity = 0.7;
+  box = MeshBuilder.CreateBox("box", { size: 2 }, scene);
+  box.position.y = 1;
+  MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
+};
+
+const onRender = (scene) => {
+  if (box !== undefined) {
+    const deltaTimeInMillis = scene.getEngine().getDeltaTime();
+
+    const rpm = 10;
+    box.rotation.y += (rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000);
+  }
+};
+
+export default () => (
+  <div>
+    <Secene antialias onSceneReady={onSceneReady} onRender={onRender} id="my-canvas" />
+  </div>
+);
