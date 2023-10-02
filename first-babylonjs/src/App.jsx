6eccc05e72e89447e59c6628a00a083561e67cad
seulgi <guseul9405@gmail.com>
2023-10-02 12:18:38 +0900

    바빌론 리액트 wrapper 예제

--- first-babylonjs/src/App.jsx
+++ first-babylonjs/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 import MostFirst from "./examples/MostFirst";
+import BabylonReactEx from "./examples/BabylonReactEx";
 
 function App() {
   return (
     <>
       <h1>바빌론 라이브러리 사용 예제</h1>
-      <MostFirst />
+      {/* <MostFirst /> */}
+      <BabylonReactEx />
     </>
   );
 }
