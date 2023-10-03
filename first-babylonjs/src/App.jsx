b5b515c67055bcf0e9fe1a309005a667ed421de5
seulgi <guseul9405@gmail.com>
2023-10-03 14:18:24 +0900

    Feat: 기본 씬, 빛, 카메라, 그라운드, 박스 처리

--- first-babylonjs/src/App.jsx
+++ first-babylonjs/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from "react";
 import "./App.css";
 import MostFirst from "./examples/MostFirst";
 import BabylonReactEx from "./examples/BabylonReactEx";
+import ClickEvent from "./examples/ClickEvent";
+import BasicScene from "./examples/BasicScene";
 
 function App() {
   return (
     <>
       <h1>바빌론 라이브러리 사용 예제</h1>
       {/* <MostFirst /> */}
-      <BabylonReactEx />
+      {/* <BabylonReactEx /> */}
+      {/* <ClickEvent /> */}
+      <BasicScene />
     </>
   );
 }
